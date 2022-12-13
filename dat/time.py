1dd5d5d1b574b70c8715984601b43814cff3359a
Satish Raju <satishbraju@gmail.com>
2022-12-13 15:34:55 +0100

    updated version

---
+++ dat/time.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+localtime = time.asctime( time.localtime(time.time()) )
+
+file = "C:/git_ws/BbB/dat/time.txt"
+
+i = 1
+
+while (i <= 100):
+    print(i)
+    source =  open(file, 'a' )
+    temp = localtime
+    print(temp, file=source)
+    i += 1
+    os.system('git add . ')
+    os.system('git commit -m "updated version')
